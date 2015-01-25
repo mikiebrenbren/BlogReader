88cc3fc3906e8d9f4bd4aa99e246679060eb977b
Michael Brennan <mikiebrenbren@gmail.com>
2015-01-24 22:01:14 -0700

    added some data to the string resources, changed the way teh mainlistactivity handles said data

--- app/src/main/java/com/veechie/mikiebrenbren/blogreader/MainListActivity.java
+++ app/src/main/java/com/veechie/mikiebrenbren/blogreader/MainListActivity.java
@@ -1,17 +1,30 @@
 package com.veechie.mikiebrenbren.blogreader;
 
 import android.app.ListActivity;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 
 
 public class MainListActivity extends ListActivity {
+
+    protected String[] mAndroidNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
+
+        Resources resources = getResources();
+        mAndroidNames = resources.getStringArray(R.array.array_names);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, mAndroidNames);
+        setListAdapter(adapter);
+//        String message = getString(R.string.no_items);
+//        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+
     }
 
 

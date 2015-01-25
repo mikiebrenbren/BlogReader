9e0558795ca247c5c7fcd9958122a313ca9faf44
Michael Brennan <mikiebrenbren@gmail.com>
2015-01-25 10:15:26 -0700

    an async task in mainlistactivity, added an internet permission to manifest

--- app/src/main/java/com/veechie/mikiebrenbren/blogreader/MainListActivity.java
+++ app/src/main/java/com/veechie/mikiebrenbren/blogreader/MainListActivity.java
@@ -1,27 +1,38 @@
 package com.veechie.mikiebrenbren.blogreader;
 
 import android.app.ListActivity;
-import android.content.res.Resources;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class MainListActivity extends ListActivity {
 
-    protected String[] mAndroidNames;
+    protected String[] mBlogPostTitles;
+
+    public static final int NUMBER_OF_POSTS = 20;
+    public static final String TAG = MainListActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
 
-        Resources resources = getResources();
-        mAndroidNames = resources.getStringArray(R.array.array_names);
+//        Resources resources = getResources();
+//        mBlogPostTitles = resources.getStringArray(R.array.array_names);
+//
+//        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, mBlogPostTitles);
+//        setListAdapter(adapter);
+          GetBlogPostsTask getBlogPostsTasks = new GetBlogPostsTask();
+          getBlogPostsTasks.execute();  //do not call doinbackground method
 
-        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, mAndroidNames);
-        setListAdapter(adapter);
 //        String message = getString(R.string.no_items);
 //        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
 
@@ -34,7 +45,6 @@ public class MainListActivity extends ListActivity {
         getMenuInflater().inflate(R.menu.menu_main_list, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -49,4 +59,32 @@ public class MainListActivity extends ListActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private class GetBlogPostsTask extends AsyncTask<Object, Void, String> {
+
+        int responseCode = -1;
+        @Override
+        protected String doInBackground(Object[] params) {
+            try {
+                URL blogFeedUrl = new URL(" http://blog.teamtreehouse.com/api/get_recent_summary/?count=" + NUMBER_OF_POSTS);
+                HttpURLConnection connection =(HttpURLConnection) blogFeedUrl.openConnection();
+                connection.connect();
+
+                responseCode = connection.getResponseCode();
+                Log.i(TAG,"Code: " + responseCode);
+
+            }catch(MalformedURLException e){
+                Log.e(TAG, "Exception Caught: ", e);
+            }catch (IOException e){
+                Log.e(TAG, "Exception Caught: ", e);
+            }catch(Exception e){
+                Log.e(TAG, "Exception Caught: ", e);
+            }
+                return "Code: " + responseCode;
+        }
+    }
 }
+
+
+
+

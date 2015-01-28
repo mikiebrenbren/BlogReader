f57832b92304a7c6c389d26ecc682d2ff39ee8e9
Michael Brennan <mikiebrenbren@gmail.com>
2015-01-27 22:16:00 -0700

    added the input/json reader to AsyncTask in mainlistactivity, added a new permission inside of manifest to allow viewing of connection state

--- app/src/main/java/com/veechie/mikiebrenbren/blogreader/MainListActivity.java
+++ app/src/main/java/com/veechie/mikiebrenbren/blogreader/MainListActivity.java
@@ -1,13 +1,23 @@
 package com.veechie.mikiebrenbren.blogreader;
 
 import android.app.ListActivity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -30,12 +40,27 @@ public class MainListActivity extends ListActivity {
 //
 //        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, mBlogPostTitles);
 //        setListAdapter(adapter);
-          GetBlogPostsTask getBlogPostsTasks = new GetBlogPostsTask();
-          getBlogPostsTasks.execute();  //do not call doinbackground method
-
+        if(isNetworkAvailable()) {
+            GetBlogPostsTask getBlogPostsTasks = new GetBlogPostsTask();
+            getBlogPostsTasks.execute();  //do not call doinbackground method
+        }else {
+            Toast.makeText(this, "Network is unavailable!", Toast.LENGTH_LONG).show();
+        }
 //        String message = getString(R.string.no_items);
 //        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
 
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+        boolean isAvailable = false;
+        if(networkInfo !=null && networkInfo.isConnected()) {
+            isAvailable = true;
+        }
+        return isAvailable;
     }
 
 
@@ -71,8 +96,27 @@ public class MainListActivity extends ListActivity {
                 connection.connect();
 
                 responseCode = connection.getResponseCode();
-                Log.i(TAG,"Code: " + responseCode);
+                if(responseCode == HttpURLConnection.HTTP_OK){
+                    InputStream inputStream =  connection.getInputStream();
+                    Reader reader = new InputStreamReader(inputStream);
+                    int contentLength = connection.getContentLength();
+                    char[] charArray = new char[contentLength];
+                    reader.read(charArray);
+                    String responseData = new String(charArray);
 
+                    //get json
+                    JSONObject jsonResponse = new JSONObject(responseData);
+                    String status = jsonResponse.getString("status");
+                    Log.v(TAG,status);
+                    JSONArray jsonPosts = jsonResponse.getJSONArray("posts");
+                    for(int i = 0; i < jsonPosts.length(); i++){
+                        JSONObject jsonPost = jsonPosts.getJSONObject(i);
+                        String title = jsonPost.getString("title");
+                        Log.v(TAG, "Post " + i + " " + title);
+                    }
+                }else {
+                    Log.i(TAG, "Unsuccessful HTTP Response Code: " + responseCode);
+                }
             }catch(MalformedURLException e){
                 Log.e(TAG, "Exception Caught: ", e);
             }catch (IOException e){
